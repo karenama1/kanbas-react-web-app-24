84223425fadb452e50f9e67b996b08664bf34390
karenama1 <karenama329@gmail.com>
2024-04-26 12:22:16 -0700

    modified small syntax error

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -2,6 +2,7 @@ import Signin from "../../Users/Signin";
 import Profile from "../../Users/Profile";
 import { Routes, Route, Navigate } from "react-router-dom";
 import UserTable from "../../Users/Table";
+import Signup from "../../Users/Signup";
 export default function Account() {
   return (
     <div className="container-fluid">
@@ -10,6 +11,7 @@ export default function Account() {
         <Route path="/Signin" element={<Signin />} />
         <Route path="/Profile" element={<Profile />} />
         <Route path="/Admin/Users" element={<UserTable />} />
+        <Route path="/Signup" element={<Signup />} />
       </Routes>
     </div>
   );
